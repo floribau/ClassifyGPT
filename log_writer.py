63c59a20dbfe8c2953421097ca94704a8dd396f3
Florian Bauer <fpb255@gmx.de>
2024-03-31 20:21:15 +0200

    add LogWriter

---
+++ log_writer.py
@@ -0,0 +1,34 @@
+import datetime
+
+
+class LogWriter:
+    """
+    Class that provides functionality to write log files
+    """
+    def __init__(self):
+        """
+        Opens a new log file corresponding to the current date and time
+        """
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.strftime('%Y-%m-%d-%H-%M-%S')
+        logfile_name = "Logs/log_" + formatted_time + ".txt"
+
+        self.log_file = open(logfile_name, "w")
+
+    def write(self, message):
+        """
+        Writes to the log file
+
+        :param message: String - Message to be written to the logfile
+        """
+        if not self.log_file:
+            raise Exception("No log file")
+        self.log_file.write(message + '\n')
+
+    def close(self):
+        """
+        Closes the log file
+        """
+        if self.log_file:
+            self.log_file.close()
+            self.log_file = None
